67d297aa842e5148ac738e147069fc4bebe9e6ac
Hi Gaming DE2000 <128186675+HiGamingDE2000@users.noreply.github.com>
2024-06-04 18:47:08 +0200

    Add files via upload

---
+++ de/dashboard/index.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'search' => 'Suche nach Servern...',
+    'no_matches' => 'There were no servers found matching the search criteria provided.',
+    'cpu_title' => 'CPU',
+    'memory_title' => 'Memory',
+];
